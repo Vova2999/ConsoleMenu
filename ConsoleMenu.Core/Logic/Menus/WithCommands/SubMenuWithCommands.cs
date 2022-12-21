2b7cace285aef75803b1d23ebe0c91714fb6ac82
Vova2999 <Vova2999@gmail.com>
2022-12-21 20:02:05 +0400

    Add dynamic menu description

--- ConsoleMenu.Core/Logic/Menus/WithCommands/SubMenuWithCommands.cs
+++ ConsoleMenu.Core/Logic/Menus/WithCommands/SubMenuWithCommands.cs
@@ -5,13 +5,23 @@ namespace ConsoleMenu.Core.Logic.Menus.WithCommands {
 		protected override string BackCommandDescription => "Назад";
 
 		public string Description { get; }
+		public Func<TValue, string> GetDescription { get; }
 
 		public SubMenuWithCommands(string description, params ICommand<TValue>[] commands) : base(commands) {
 			Description = description;
 		}
 
+		public SubMenuWithCommands(Func<TValue, string> getDescription, params ICommand<TValue>[] commands) : base(commands) {
+			GetDescription = getDescription;
+		}
+
+		public SubMenuWithCommands(string description, Func<TValue, string> getDescription, params ICommand<TValue>[] commands) : base(commands) {
+			Description = description;
+			GetDescription = getDescription;
+		}
+
 		protected override void PrintCommands(TValue value) {
-			Console.WriteLine($"<{Description}>");
+			Console.WriteLine($"<{GetDescription?.Invoke(value) ?? Description}>");
 			base.PrintCommands(value);
 		}
 	}
